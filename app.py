6e53b78a625d0c2a2e68e20c544eb448762c3393
César Cortez <52660278+CesarCort@users.noreply.github.com>
2021-05-07 17:11:01 -0500

    Update app.py
    change app.run

--- app.py
+++ app.py
@@ -96,7 +96,7 @@ def download():
     return send_file(memory_file,attachment_filename=fileName,as_attachment=True)
     
 
-if __name__ == "__main__":
-    # excel.init_excel(app)
-    app.run(debug=False)
+if __name__=="__main__":
+    port = int(os.environ.get("PORT",5000))
+    app.run(host="0.0.0.0",port=port)
 
